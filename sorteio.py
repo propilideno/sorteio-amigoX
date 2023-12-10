d6d1bbe08c0f1ff2cce36e4cfab655628b594d45
lucas <lucas@propi.dev>
2023-12-09 22:13:52 -0300

    lendo as pessoas de um arquivo.txt

--- sorteio.py
+++ sorteio.py
@@ -1,9 +1,12 @@
 from random import shuffle
 
-pessoas = ['João', 'Maria', 'José', 'Pedro']
+with open('pessoas.txt', 'r') as f:
+# Read all lines of file, split by \n and ignore all empty lines
+    pessoas = f.read().strip().split('\n')
+
 shuffle(pessoas)
 sorteio = {x:y for x, y in zip(pessoas, pessoas[1:] + pessoas[:1])}
 
-print('Sorteio: ')
+print('=== Sorteio ===')
 for k, v in sorteio.items():
     print(f'{k} tirou {v}')
